730b4b2346222d28c035e2399c6320d68f17b6e1
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-08-26 23:43:50 +0300

    add

--- lib/riveropd/product_list_riverpod.dart
+++ lib/riveropd/product_list_riverpod.dart
@@ -2,12 +2,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hala_sat_task/core/models/product_list_models.dart';
 import 'package:hala_sat_task/core/repository/product_list_repository.dart';
 
-final productListFutureProvider = FutureProvider.autoDispose((ref) {
-  Future<Products> products;
-  final ProductListRepository productListRepository = ProductListRepository();
+// final productListFutureProvider = FutureProvider.autoDispose((ref) {
+//   Future<Products> products;
+//   final ProductListRepository productListRepository = ProductListRepository();
 
-  products = productListRepository.geProductLitModels();
-  print("rever${products}");
-  print('hi from riverpod');
-  return products;
+//   products = productListRepository.geProductLitModels();
+//   print("rever${products}");
+//   print('hi from riverpod');
+//   return products;
+// });
+final productListFutureProvider = FutureProvider.autoDispose((ref) async {
+  try {
+    final ProductListRepository productListRepository = ProductListRepository();
+    return await productListRepository.geProductLitModels();
+  } catch (e) {
+    return Future.error(
+        'An error occurred. Please try again later.'); // Return a user-friendly error message
+  }
 });
