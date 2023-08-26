51a239de18e42acd1547d4d9553d82d2e16e7ab2
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-08-26 11:30:08 +0300

    widgit file init

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,32 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:simple_circular_progress_bar/simple_circular_progress_bar.dart';
+
+import '../constance/constance.dart';
+
+/// remove blue light
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildOverscrollIndicator(
+      BuildContext context, Widget child, ScrollableDetails details) {
+    return child;
+  }
+}
+
+ClipRRect MyCashedNetworkImage({required image}) {
+  return ClipRRect(
+    borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
+    child: CachedNetworkImage(
+      fit: BoxFit.cover,
+      imageUrl: image,
+      placeholder: (context, url) => const SimpleCircularProgressBar(
+        progressStrokeWidth: 5,
+        backStrokeWidth: 5,
+        size: 20,
+        progressColors: [AppColors.purpleMain],
+      ),
+      errorWidget: (context, url, error) => const Icon(Icons.error),
+    ),
+  );
+}
