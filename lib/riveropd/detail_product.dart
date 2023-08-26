1f09704191af23830089a13c0b064ba93bfe7d4f
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-08-26 18:23:38 +0300

    detailproduct reverpod

--- lib/riveropd/detail_product.dart
+++ lib/riveropd/detail_product.dart
@@ -1,1 +1,15 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hala_sat_task/core/models/detail_product.dart';
+import 'package:hala_sat_task/core/repository/detail_product.dart';
 
+final mealsFutureProvider = FutureProvider.autoDispose((ref) {
+  Future<DetailProduct> detailProduct;
+  final DetailProductRepository detailProductRepository =
+      DetailProductRepository(ref.watch(idProvider.notifier).state ?? '');
+
+  detailProduct = detailProductRepository.getDetailProductModel();
+  print("i am here 3");
+  return detailProduct;
+});
+
+final idProvider = StateProvider<String?>((ref) => null);
