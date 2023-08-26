9908aa57d70d71c95f50496b22c763c96dba8df0
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-08-26 23:43:34 +0300

    add

--- lib/core/repository/product_list_repository.dart
+++ lib/core/repository/product_list_repository.dart
@@ -14,6 +14,7 @@ class ProductListRepository {
       print('hi from repo');
       return products;
     } catch (e) {
+      print('Error: $e'); // Print the error for debugging
       rethrow;
     }
   }
