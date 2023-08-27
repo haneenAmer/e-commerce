1f6ea6ef9230ffed89a48eec5410a9f0cf0c49ef
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-08-27 17:08:31 +0300

    edites

--- lib/riveropd/product_list_riverpod.dart
+++ lib/riveropd/product_list_riverpod.dart
@@ -1,22 +1,18 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hala_sat_task/core/models/product_list_models.dart';
 import 'package:hala_sat_task/core/repository/product_list_repository.dart';
+import 'package:riverpod_infinite_scroll/riverpod_infinite_scroll.dart';
 
-// final productListFutureProvider = FutureProvider.autoDispose((ref) {
-//   Future<Products> products;
-//   final ProductListRepository productListRepository = ProductListRepository();
+class ProductsNotifier extends PagedNotifier<int, Product> {
+  ProductsNotifier()
+      : super(
+          load: (page, pageLimitimit) =>
+              ProductListRepository().geProductLitModels(page, pageLimitimit),
+          nextPageKeyBuilder: NextPageKeyBuilderDefault.mysqlPagination,
+        );
+  final ProductListRepository productListRepository = ProductListRepository();
+}
 
-//   products = productListRepository.geProductLitModels();
-//   print("rever${products}");
-//   print('hi from riverpod');
-//   return products;
-// });
-final productListFutureProvider = FutureProvider.autoDispose((ref) async {
-  try {
-    final ProductListRepository productListRepository = ProductListRepository();
-    return await productListRepository.geProductLitModels();
-  } catch (e) {
-    return Future.error(
-        'An error occurred. Please try again later.'); // Return a user-friendly error message
-  }
-});
+final productsProvider =
+    StateNotifierProvider<ProductsNotifier, PagedState<int, Product>>(
+        (_) => ProductsNotifier());
