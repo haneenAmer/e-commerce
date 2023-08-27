01d216192e9bc4f8cae61e3b2b6b9b0417bd2579
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-08-27 16:37:57 +0300

    deail prodct ui changes

--- lib/core/repository/detail_product.dart
+++ lib/core/repository/detail_product.dart
@@ -1,21 +1,26 @@
-import 'package:hala_sat_task/core/models/detail_product.dart';
+import 'dart:developer';
 
+import 'package:dio/dio.dart';
+import 'package:hala_sat_task/core/models/detail_product.dart';
+import '../../network/dio_exeptions.dart';
 import '../services/detail_product.dart';
 
 class DetailProductRepository {
-  final String id;
+  final int id;
   DetailProductRepository(this.id);
 
   Future<DetailProduct> getDetailProductModel() async {
-    DetailProductServices detailProductServices =
-        DetailProductServices('https://dummyjson.com/products', id);
+    DetailProductServices detailProductServices = DetailProductServices(id);
 
     try {
       final json = await detailProductServices.detailProducResponse();
+
       DetailProduct detailProduct = DetailProduct.fromJson(json);
       //print(" i am here 2${detailProduct}");
       return detailProduct;
-    } catch (e) {
+    } on DioException catch (e) {
+      final errorMessage = DioExceptions.fromDioError(e);
+      log(errorMessage.toString());
       rethrow;
     }
   }
