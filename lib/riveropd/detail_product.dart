4c79d48e33ebb23b77b68912ada3d4d61f40c195
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-08-27 17:08:21 +0300

    edites

--- lib/riveropd/detail_product.dart
+++ lib/riveropd/detail_product.dart
@@ -3,13 +3,12 @@ import 'package:hala_sat_task/core/models/detail_product.dart';
 import 'package:hala_sat_task/core/repository/detail_product.dart';
 
 final productFutureProvider = FutureProvider.autoDispose((ref) {
-  Future<DetailProduct> detailProduct;
+  final Future<DetailProduct> detailProduct;
   final DetailProductRepository detailProductRepository =
-      DetailProductRepository(ref.watch(idProvider.notifier).state ?? '');
+      DetailProductRepository(ref.read(idProvider.notifier).state!);
 
   detailProduct = detailProductRepository.getDetailProductModel();
-  print("i am here 3");
   return detailProduct;
 });
 
-final idProvider = StateProvider<String?>((ref) => null);
+final idProvider = StateProvider<int?>((ref) => null);
