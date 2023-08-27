076bb523ed1b1ac7a67bfebd319988c38223d5e0
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-08-28 00:25:56 +0300

    edites

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,5 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_circular_progress_bar/simple_circular_progress_bar.dart';
 
@@ -14,9 +13,9 @@ class MyBehavior extends ScrollBehavior {
   }
 }
 
-ClipRRect MyCashedNetworkImage({required image}) {
+ClipRRect myCashedNetworkImage({required image}) {
   return ClipRRect(
-    borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
+    borderRadius: const BorderRadius.vertical(top: Radius.circular(8)),
     child: CachedNetworkImage(
       fit: BoxFit.cover,
       imageUrl: image,
